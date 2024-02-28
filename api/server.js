3f7141744b05ce4e4a4c447a4e463d09a2bff6ff
mkaufman385 <mkaufman385@gmail.com>
2024-02-28 16:45:44 -0500

    Wednesday night push

--- api/server.js
+++ api/server.js
@@ -1,6 +1,19 @@
 const express = require("express");
-
+const User = require("./users/model");
 const server = express();
+
+server.get("/api/users", (req, res) => {
+  User.find()
+    .then((users) => {
+      throw new Error("noooo");
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "error getting users",
+        err: err.message,
+      });
+    });
+});
 
 server.use("*", (req, res) => {
   res.status(404).json({
