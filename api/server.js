e7afd4b0ebc41542a5e9b709dc3b2da8acb4a5b9
mkaufman385 <mkaufman385@gmail.com>
2024-02-29 13:12:11 -0500

    delete working

--- api/server.js
+++ api/server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const User = require("./users/model");
-const server = express();
 
+const server = express();
 server.use(express.json());
+
+server.delete("/api/users/:id", async (req, res) => {
+  const possibleUser = await User.findById(req.params.id);
+  if (!possibleUser) {
+    res.status(404).json({
+      message: "not found",
+    });
+  } else {
+    const deletedUser = await User.remove(possibleUser.id);
+    res.status(200).json(deletedUser);
+  }
+});
 
 server.post("/api/users", (req, res) => {
   const user = req.body;
