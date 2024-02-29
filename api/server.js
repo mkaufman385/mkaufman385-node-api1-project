46a18fd97936d63a7e2a6cdce645b9051b11b405
mkaufman385 <mkaufman385@gmail.com>
2024-02-29 12:32:39 -0500

    get users and get user by id

--- api/server.js
+++ api/server.js
@@ -5,12 +5,27 @@ const server = express();
 server.get("/api/users", (req, res) => {
   User.find()
     .then((users) => {
-      throw new Error("noooo");
+      res.json(users);
     })
     .catch((err) => {
       res.status(500).json({
         message: "error getting users",
         err: err.message,
+        stack: err.stack,
+      });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "error getting users",
+        err: err.message,
+        stack: err.stack,
       });
     });
 });
